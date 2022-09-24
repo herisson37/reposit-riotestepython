6508b5a5f667aa59e322d2ea025e441fc8a28fab
Herrisson <herrissonvinicius@gmail.com>
2022-09-24 00:18:54 -0400

    Primeira modificação teste.

--- test2.py
+++ test2.py
@@ -1,4 +1,10 @@
-def funcTest():
-    print ("Você vai passar nessa prova!")
-    print("Você precisa passar!")
-funcTest()
+from ast import If
+
+
+def funcTest(a, b):
+
+    For x in range(5):
+        print ("Você vai passar nessa prova!")
+        print("Você precisa passar!")
+
+funcTest(5, 4)
