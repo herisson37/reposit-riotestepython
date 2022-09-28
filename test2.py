dd0e0cadf5670202dca68e737b5eb0c9e1169365
Herrisson <herrissonvinicius@gmail.com>
2022-09-27 23:38:26 -0400

    Métodos de string.

--- test2.py
+++ test2.py
@@ -1,10 +1,55 @@
-from ast import If
+from ast import For, If
+
+#def funcTest(a, b):
+#    For n in range(5):
+#        print (n)
+
+#funcTest(5, 4)
+
+#for a in range(5):
+#    print(a)
+
+def funcTest2(a):
+    for x in range(6):
+        
+        if x > a :
+            break
+        else:
+            print(x)
+
+texto = "Jesus"
+
+def funcStrings():
+
+    ultimo = len(texto) - 1
+    print(texto[ultimo])
+    
+    #Métodos de string
+    metodoFind = texto.find("u")
+    print("Método .find: ")
+    print(metodoFind)
+
+    metodoJoin = ">".join(texto)
+    print("Método .join: ")
+    print(metodoJoin)
+
+    metodoSplit = texto.split("e")
+    print("Método .split: ")
+    print(metodoSplit)
+
+    metodoReplace = texto.replace("s", "S")
+    print("Método .replace: ")
+    print(metodoReplace)
+
+#funcStrings()
 
 
-def funcTest(a, b):
+def desafioOrdemAlfabetica(palavra1, palavra2):
+    if palavra1.upper() < palavra2.upper():
+        print(palavra1)
+        print(palavra2)
+    else:
+        print(palavra2)
+        print(palavra1)
 
-    For x in range(5):
-        print ("Você vai passar nessa prova!")
-        print("Você precisa passar!")
-
-funcTest(5, 4)
+desafioOrdemAlfabetica("Xicara", "banana")
